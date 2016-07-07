b80cc45fa5f18c6eacef7363f5ea662f9a11c1eb
Timothy G. Rundle <timothy.rundle@volvo.com>
2016-07-07 10:37:49 -0400

    converted to dynamic library to use with Carthage

---
+++ JTNumberScrollAnimation/JTNumberScrollAnimation.h
@@ -0,0 +1,19 @@
+//
+//  JTNumberScrollAnimation.h
+//  JTNumberScrollAnimation
+//
+//  Created by Timothy G. Rundle on 7/7/16.
+//  Copyright © 2016 Jonathan Tribouharet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTNumberScrollAnimation.
+FOUNDATION_EXPORT double JTNumberScrollAnimationVersionNumber;
+
+//! Project version string for JTNumberScrollAnimation.
+FOUNDATION_EXPORT const unsigned char JTNumberScrollAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTNumberScrollAnimation/PublicHeader.h>
+#import <JTNumberScrollAnimation/JTNumberScrollAnimatedView.h>
+
